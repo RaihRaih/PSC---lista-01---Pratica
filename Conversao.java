2d3d37a750010a811eca0d7d9998b65e2329d6ad
Raissa Rodrigues <105363135+RaihRaih@users.noreply.github.com>
2024-03-06 12:47:37 -0300

    Update Conversao.java

--- Conversao.java
+++ Conversao.java
@@ -1,24 +1,24 @@
-
 /*Raissa Rodrigues - Sistemas de Informação
  * 5 - Crie um algoritmo que lê um valor real em dólar, e converte o valor para reais. Considere que a cotação é US$ 1 = R$ 4,95.
  */
 
 import java.util.Scanner;
 
-public class Conversao {
+public class ConversaoDolarReal {
     public static void main(String[] args) {
-        
-        System.out.print("Digite um valor em dolar e veja a sua conversão em real (Dolar Norte Americano -> Real Brasileiro):\n");
+
+        System.out.print(
+                "Digite um valor em dolar e veja a sua conversão em real (Dolar Norte Americano -> Real Brasileiro):\n");
 
         double real, dolar;
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.println("Dolar:");
         dolar = scanner.nextFloat();
-        real =  dolar * 4.95;
+        real = dolar * 4.95;
 
-        System.out.println("resto da divisao: "+ real);
-    
+        System.out.println("Conversão em Reais: " + real);
+
         scanner.close();
     }
 }
